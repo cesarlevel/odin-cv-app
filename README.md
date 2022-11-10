ab9fe772b7de8453c07c9b210967fdd355ca2d37
Cesar Level <cesarlevel@gmail.com>
2022-11-10 12:28:27 -0500

    gh-pages

--- README.md
+++
@@ -1,4 +0,0 @@
-# odin-cv-app
-The Odin project - CV app
-
-[Live demo](https://cesarlevel.github.io/odin-cv-app/)
--- src/composables/use-add-remove-array-items.js
+++
@@ -1,7 +0,0 @@
-export function addStateArrayItem(state, item) {
-    state.push(item);
-}
-
-export function deleteStateArrayItem(state, itemIndex) {
-    state.splice(itemIndex, 1);
-}
